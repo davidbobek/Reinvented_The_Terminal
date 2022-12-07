14f7d91587819e7c6b46b9ff75be15fc37795ae6
root <root@Dinu_Zenbook>
2022-12-07 13:13:29 +0100

    bullshit

--- src/mine.c
+++ src/mine.c
@@ -16,7 +16,6 @@
 int changeDirectory(char* args[]){
 	// If we write no path (only 'cd'), then go to the home directory
 	if (args[1] == NULL) {
-        
 		chdir(getenv("HOME")); 
         printf("Changed to home directory\n");
 		return 1;
@@ -35,13 +34,13 @@ int changeDirectory(char* args[]){
     }     
 }
 
+
 int quit(){
-    // if any poccesses are running, we ask the user if he wants to quit
+    // if any poccesses are running, then
     // print all the running processes
     // ask the user if he wants to quit
     // if yes, then quit
     // if no, then continue
-    // if no processes are running, then quit
     printf("Following processes are running:\n");
     system("ps");
     printf("Are you sure you want to quit? (y/n)\n");
@@ -58,6 +57,7 @@ int quit(){
 
 }
 
+
 int globalUsage(){
     char text[] = "IMCSH Version 1.1 created by Bertold Vinze, David Bobek and Dinu Scripnic";
     printf("%s\n",text);
@@ -66,12 +66,12 @@ int globalUsage(){
 
 
 int executeFunction(char* tokens[]){
-
+    // fork a child process
     pid_t pid;
     int status;
     pid = fork();
     if (pid == 0){
-        // child process
+        // 
         if (execvp(tokens[1], tokens+1) < 0){
             printf("Error executing command\n");
             return -1;
@@ -90,43 +90,96 @@ int executeFunction(char* tokens[]){
     return 1;
 }
 
-int modifierTO(char* tokens[]){
-    FILE *fp;
+int export(char* tokens[]){
     // find the filename in tokens
     // open the file
     // write the output of the command to the file
     // close the file
     // return 1
-    
+    FILE *pFile = NULL;
+    if (strcmp(tokens[0], "globalusage") ==0){
+        pFile = fopen(tokens[2], "w");
+        if (pFile == NULL){
+            printf("Error opening file\n");
+            return -1;
+        }
+        fprintf(pFile, "IMCSH Version 1.1 created by Bertold Vinze, David Bobek and Dinu Scripnic");
+        fclose(pFile);
+        return 1;
+    }
+    else if (strcmp(tokens[0], "exec")==0){
+        // iterate through all the tokens till > is found
+        // create a new array with the tokens before >
+        // execute the command
+        // write the output to the file
+        // close the file
+        // return 1
+        char* newTokens[LIMIT];
+        int i = 0;
+        while (strcmp(tokens[i], ">") != 0){
+            newTokens[i] = tokens[i];
+            i++;
+        }
+        pid_t pid;
+        int status;
+        pid = fork();
+        if (pid == 0){
+            // child process
+            pFile = fopen(tokens[i+1], "w");
+            if (pFile == NULL){
+                printf("Error opening file\n");
+                return -1;
+            }
+            dup2(fileno(pFile), 1);
+            if (execvp(newTokens[0], newTokens) < 0){
+                printf("Error executing command\n");
+                return -1;
+            }
+    }
 
+    return 0;
+}
 }
 
 
 int commandHandler(char* tokens[]){
-
+    int i = 0;
+    // check if the command have > in it
+    // The error is in this for loop
+    for (int i = 0; i < LIMIT; i++){
+        if (strcmp(tokens[i], ">") == 0){
+            printf("Found >\n");
+            // export(tokens);
+            break;
+        }
+        if (strcmp(tokens[i], "&") == 0){
+            printf("Found &\n");
+            // run in background
+            break;
+        }
+    }
+    // check if the command is cd
     if(strcmp(tokens[0],"cd")==0){
         changeDirectory(tokens);
     }
+    // check if the command is quit
     else if(strcmp(tokens[0],"quit")==0){
         quit();
     }
+    // check if the command is exec
     else if (strcmp(tokens[0],"exec")==0){
         executeFunction(tokens);
     }
-    // global usage
+    // check if the command is globalusage
     else if (strcmp(tokens[0],"globalusage") == 0){
         globalUsage();
-    }
+    } 
+    // if none of the above, then the command is not found
     else{
         printf("Command not found\n");
     }
 }
 
-int modifierTO(char* tokens[]){
-
-}
-
-    
 
 int main() {
 	char line[MAXLINE]; // buffer for the user input
@@ -171,15 +224,15 @@ int main() {
 		numTokens = 1;
 		while((tokens[numTokens] = strtok(NULL, " \n\t")) != NULL) numTokens++;
         commandHandler(tokens);
-        int i;
-        for(i = 0; i < numTokens; i++){
+    //     int i;
+    //     for(i = 0; i < numTokens; i++){
             
-            printf("token %d: %s\n", i,tokens[i]);
+    //         printf("token %d: %s\n", i,tokens[i]);
         
         
             
 		
-	}          
+	// }          
 
 	
 }
